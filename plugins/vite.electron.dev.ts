e25b7a3d2e8ba29c5bc092ea26d824a58c44e579
afacode <afacode@outlook.com>
2023-11-17 11:32:21 +0800

    feat: main process热更新

--- plugins/vite.electron.dev.ts
+++ plugins/vite.electron.dev.ts
@@ -1,25 +1,37 @@
 import { AddressInfo } from 'node:net'
 import type {Plugin, ViteDevServer} from 'vite'
 import {spawn} from 'child_process'
-
+import fs from 'node:fs'
 
 export const ElectronDevPlugin = () :Plugin => {
     return {
         name: 'electron-dev',
         configureServer(server: ViteDevServer) {
-            require('esbuild').buildSync({
-                entryPoints: ['src/packground.ts'],
-                outfile: 'dist/packground.js',
-                bundle: true,
-                platform: 'node',
-                external: ['electron']
-            })
-            server.httpServer?.on('listening', () => {
+            buildBackground()
+            server.httpServer?.once('listening', () => {
                 const addressInfo = server.httpServer?.address() as AddressInfo
                 const IP = `http://localhost:${addressInfo.port}`
                 console.log(IP)
-                spawn(require('electron'), ['dist/packground.js', IP])
+                let ElectronProcess = spawn(require('electron'), ['dist/packground.js', IP])
+                fs.watchFile('src/packground.ts', () => {
+                    ElectronProcess.kill()
+                    buildBackground()
+                    ElectronProcess = spawn(require('electron'), ['dist/packground.js', IP])
+                })
+                ElectronProcess.stderr.on('data', (data) => {
+                    console.log('logger----', data.toString())
+                })
             })
         }
     }
+}
+
+function buildBackground() {
+    require('esbuild').buildSync({
+        entryPoints: ['src/packground.ts'],
+        outfile: 'dist/packground.js',
+        bundle: true,
+        platform: 'node',
+        external: ['electron']
+    })
 }
