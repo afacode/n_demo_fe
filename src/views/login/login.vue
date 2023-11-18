39c44b252fdd5f8b1e4bfab32ca1accadc053fe5
afacode <afacode@outlook.com>
2023-11-18 20:35:25 +0800

    feat: menu

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,62 +1,144 @@
 <template>
   <div class="login-box">
-    <h1>登录</h1>
-    <el-form ref="loginRef" class="login" :label-position="'left'" label-width="40px" :model="formLabelAlign">
-      <el-form-item label="账号">
-        <el-input v-model="formLabelAlign.username" />
-      </el-form-item>
-      <el-form-item label="密码">
-        <el-input type="password" v-model="formLabelAlign.password" />
-      </el-form-item>
-      <el-form-item>
-
-        <el-button type="primary" class="submit" @click="submitForm">登录</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="login-logo">
+      <h1 class="mb-0 ml-2 text-3xl font-bold">Antd Admin</h1>
+    </div>
+    <a-form layout="horizontal" :model="state.formInline" @submit.prevent="handleSubmit">
+      <a-form-item>
+        <a-input v-model:value="state.formInline.username" size="large" placeholder="rootadmin">
+          <template #prefix><user-outlined type="user" /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-model:value="state.formInline.password"
+          size="large"
+          type="password"
+          placeholder="123456"
+          autocomplete="new-password"
+        >
+          <template #prefix><lock-outlined type="user" /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-model:value="state.formInline.verifyCode"
+          placeholder="验证码"
+          :maxlength="4"
+          size="large"
+        >
+          <template #prefix><SafetyOutlined /></template>
+          <template #suffix>
+            <img
+              :src="state.captcha"
+              class="absolute right-0 h-full cursor-pointer"
+              @click="setCaptcha"
+            />
+          </template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" size="large" :loading="state.loading" block>
+          登录
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive } from 'vue'
-// import type { FormInstance } from 'element-plus'
-const formLabelAlign = reactive({
-  username: '',
-  password: '',
-})
-const submitForm = () => {
-  // if (!formEl) return
-  // formEl.validate(valid => {
-  //   if (valid) {
-  //     console.log('submit!')
-  //     return false
-  //   } else {
-  //     console.log('error submit!')
-  //     return false
-  //   }
-  // })
-}
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { UserOutlined, LockOutlined, SafetyOutlined } from '@ant-design/icons-vue';
+import { useRoute, useRouter } from 'vue-router';
+import { message, Modal } from 'ant-design-vue';
+// import { useUserStore } from '@/store/modules/user';
+import { getImageCaptcha } from '@/api/login';
+// import { to } from '@/utils/awaitTo';
+
+const state = reactive({
+ loading: false,
+ captcha: '',
+ formInline: {
+   username: '',
+   password: '',
+   verifyCode: '',
+   captchaId: '',
+ },
+});
+
+const route = useRoute();
+const router = useRouter();
+
+// const userStore = useUserStore();
+
+const setCaptcha = async () => {
+ const { id, img } = await getImageCaptcha({ width: 100, height: 50 });
+ state.captcha = img;
+ state.formInline.captchaId = id;
+};
+setCaptcha();
+
+const handleSubmit = async () => {
+ const { username, password, verifyCode } = state.formInline;
+ if (username.trim() == '' || password.trim() == '') {
+   return message.warning('用户名或密码不能为空！');
+ }
+ if (!verifyCode) {
+   return message.warning('请输入验证码！');
+ }
+ message.loading('登录中...', 0);
+ state.loading = true;
+ console.log(state.formInline);
+ // params.password = md5(password)
+
+//   const [err] = await to(userStore.login(state.formInline));
+//   if (err) {
+//     Modal.error({
+//       title: () => '提示',
+//       content: () => err.message,
+//     });
+//     setCaptcha();
+//   } else {
+//     message.success('登录成功！');
+//     setTimeout(() => router.replace((route.query.redirect as string) ?? '/'));
+//   }
+ state.loading = false;
+ message.destroy();
+};
 </script>
-<style lang="scss" scoped>
-.login-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
 
-  h1 {
-    font-size: 28px;
-    padding-bottom: 30px;
-    text-align: center;
-  }
 
-  .login {
-    width: 400px;
-  }
+<style lang="less" scoped>
+ .login-box {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   width: 100vw;
+   height: 100vh;
+   padding-top: 240px;
+  //  background: url('@/assets/login.svg');
+   background-size: 100%;
 
-  .submit {
-    width: 100%;
-  }
-}
+   .login-logo {
+     display: flex;
+     align-items: center;
+     margin-bottom: 30px;
+
+     .svg-icon {
+       font-size: 48px;
+     }
+   }
+
+   :deep(.ant-form) {
+     width: 400px;
+
+     .ant-col {
+       width: 100%;
+     }
+
+     .ant-form-item-label {
+       padding-right: 6px;
+     }
+   }
+ }
 </style>
