d21e898117d1d2fc6e2fdec89ce5a1a347a4b7a1
afacode <afacode@outlook.com>
2023-12-01 19:38:39 +0800

    Feat: electron main process

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 // main process
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, contentTracing } from 'electron'
 
 function createWindow() {
   // 创建浏览器窗口
@@ -10,7 +10,10 @@ function createWindow() {
       nodeIntegration: true, // 渲染进程可使用node api
       contextIsolation: false, // 关闭渲染进程的沙箱
       webSecurity: false // 关闭跨域检测
-    }
+    },
+    // backgroundColor: '#2e2c29',
+    // show: false,
+    // frame: false, // 无边框窗口
   })
 
   // 加载 Vue 应用的 index.html 文件
@@ -23,13 +26,30 @@ function createWindow() {
   }
 }
 app.whenReady().then(() => {
+  // 从Chromium收集追踪数据以找到性能瓶颈和慢操作
+  // https://www.electronjs.org/zh/docs/latest/api/content-tracing
+  (async ()=> {
+    await contentTracing.startRecording({
+      included_categories: ['*']
+    })
+    console.log('Tracing started')
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    const path = await contentTracing.stopRecording()
+    console.log('追踪数据记录到： ' + path)
+  })()
+
   createWindow()
+
+  // app.isReady()
+  // app.hide() isHidden()
+  // app.show()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+// 当所有的窗口都被关闭时触发
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
